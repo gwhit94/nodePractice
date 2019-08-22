9837faa7e983ad9bfc4bc8582c9697ae859582a1
Graham <greysnaa@gmail.com>
2019-08-22 15:32:57 -0500

    Second Commit

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
-// function yell(str){
-//     return str.split("").join(" ").toUpperCase();
-// }
+module.exports.yell = function yell(str){
+    return str.split("").join(" ").toUpperCase();
+}
 
 module.exports.randomRange = function randomRange(a,b){
     return Math.floor(Math.random() * (Math.max(a,b) - Math.min(a,b) + 1)) + Math.min(a,b);
--- torun.js
+++ torun.js
@@ -1,4 +1,9 @@
 let randomRange = require('./function').randomRange;
+let yell = require('./function').yell;
+
+let a = process.argv[2];
+let b = process.argv[3];
+let str = process.argv[4];
 
 
-console.log(randomRange(process.argv[2], process.argv[3]));
+console.log(randomRange(a,b), yell(str));
